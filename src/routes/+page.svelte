35782eb58379787db238f2ffd13db00e9a78b83d
choke-dev <admin@choke.dev>
2025-01-22 23:24:23 +0800

    feat(main page): add clear search bar button, add automatic keyword query param thing idfk, and add fetched audio count

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,61 @@
 <script lang="ts">
-	import * as Table from '$lib/components/ui/table/index';
 	import { Button } from '$lib/components/ui/button';
 	import { Input } from '$lib/components/ui/input';
 	import * as Pagination from '$lib/components/ui/pagination/index';
+	import * as Table from '$lib/components/ui/table/index';
+	
 	import LucideCircleAlert from '~icons/lucide/circle-alert';
-
-	import LucideSearch from '~icons/lucide/search';
 	import LucideLoaderCircle from '~icons/lucide/loader-circle';
+	import LucideSearch from '~icons/lucide/search';
+	import LucideX from '~icons/lucide/x';
 
+	import SearchFilter from '$lib/components/ui/custom/SearchFilter.svelte';
+	import { MAX_SEARCH_RESULTS_PER_PAGE } from '$lib/config';
 	import type { Audio } from '@prisma/client';
 	import { onMount } from 'svelte';
-	import { MAX_SEARCH_RESULTS_PER_PAGE } from '$lib/config';
-	import SearchFilter from '$lib/components/ui/custom/SearchFilter.svelte';
+	import { page } from '$app/state';
+	import { goto } from '$app/navigation';
 
 	let errors = $state<{ message: string }[]>([]);
 	let searchResults: Audio[] = $state([]);
 	let loading = $state(false);
 
 	let started = $state(false);
+	let lastSearchKeyword = $state('');
 	let keyword = $state('');
 	let currentPage = $state(1); // Track current page
-	// svelte-ignore non_reactive_update
 	let totalItems = $state(0); // Total number of items (total audios found)
 	let filters = $state<{ label: string; value: string; inputValue: string }[]>([]);
 
 	async function handleSearch(event?: Event) {
-		started = true;
+		event?.preventDefault();
 
+		started = true;
 		loading = true;
         errors = []
 		searchResults = [];
+		
 		if (event && event.target instanceof HTMLFormElement) {
-			const formData = new FormData(event.target as HTMLFormElement);
-			keyword = formData.get('keyword') as string;
+			const formData = new FormData(event.target);
+			const formKeyword = formData.get('keyword');
+			if (formKeyword) {
+				keyword = formKeyword.toString();
+			} else {
+				keyword = '';
+			}
 		}
-		const response = await fetch(`/?keyword=${encodeURIComponent(keyword)}&page=${currentPage}`, {
+		let query: URLSearchParams | undefined = new URLSearchParams(page.url.searchParams.toString());
+		if (keyword.length <= 0) {
+			query.delete('keyword');
+			goto(`?${query.toString()}`);
+		} else {
+			query.set('keyword', keyword);
+			goto(`?${query.toString()}`);
+		}
+		query = undefined;
+
+		lastSearchKeyword = keyword;
+		const response = await fetch(`/?keyword=${encodeURIComponent(lastSearchKeyword)}&page=${currentPage}`, {
 			method: 'POST',
 			body: JSON.stringify(filters)
 		});
@@ -45,7 +66,6 @@
 			return;
 		}
 		const data = await response.json();
-		console.log(data?.errors);
 		searchResults = data.items; // Assuming the response includes a `items` field with the audios
 		totalItems = data.total; // Assuming the response includes a `total` field with the total number of audios
 		loading = false;
@@ -100,7 +120,22 @@
 	<div class={`relative mx-auto mb-8 flex w-full items-center justify-between gap-x-2 ${ errors.length > 0 ? '' : 'mt-32' }`}>
 		<form onsubmit={handleSearch} class="flex w-full max-w-lg items-center gap-x-2">
 			<div class="flex flex-grow items-center gap-x-2">
-				<Input bind:value={keyword} name="keyword" placeholder="Search..." class="flex-grow" />
+				<div class="flex relative flex-grow">
+					<Input bind:value={keyword} name="keyword" type="search" placeholder="Search..." class="w-full pr-10" />
+					{#if lastSearchKeyword.length > 0}
+						<Button 
+						variant="ghost" 
+						size="icon"
+						class="absolute right-0 top-0 h-full rounded-none rounded-br-lg rounded-tr-lg"
+						onclick={() => {
+							keyword = ''
+							handleSearch()
+						}}
+						>
+							<LucideX />
+						</Button>
+					{/if}
+				</div>
 				<Button size="icon" type="submit" disabled={loading} class="flex-none">
 					{#if loading}
 						<LucideLoaderCircle class="animate-spin" />
@@ -115,8 +150,13 @@
 </div>
 
 
-<div class="mb-32 flex items-center justify-center">
-	<div class="relative z-[1] w-full max-w-[75%]">
+<div class="mb-32 mx-auto max-w-[75%] flex flex-col items-center justify-center relative">
+	<div class="w-full absolute -top-0 left-0">
+    {#if started && !loading}
+        <p class="text-zinc-500">Fetched {searchResults.length} audio{Math.abs(searchResults.length) === 1 ? '' : 's'}</p>
+    {/if}
+	</div>
+	<div class="w-full mt-6">
 		<Table.Root class="rounded-lg border backdrop-blur-sm">
 			<Table.Header class="rounded-lg">
 				<Table.Row>
@@ -183,3 +223,4 @@
 		</Pagination.Root>
 	</div>
 </div>
+
