ac922a63d5e1b86c8e2a15cc39232f5947a75aee
choke-dev <admin@choke.dev>
2025-02-06 22:46:19 +0800

    fix: 405 on base path causing meta tags to not show up

--- packages/site/src/routes/+layout.server.ts
+++ packages/site/src/routes/+layout.server.ts
@@ -1,5 +1,5 @@
 import { loadFlash } from 'sveltekit-flash-message/server';
-import { _limiter } from './+server';
+import { _limiter } from './+page.server';
 
 export const load = loadFlash(async (event) => {
     await _limiter.cookieLimiter?.preflight(event);
