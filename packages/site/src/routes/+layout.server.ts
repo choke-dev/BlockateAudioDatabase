1ac482da20419451c6545a50ae4f2e760da84370
choke-dev <admin@choke.dev>
2025-04-18 07:49:19 +0800

    remove vercel commit env var (does not work)

--- packages/site/src/routes/+layout.server.ts
+++ packages/site/src/routes/+layout.server.ts
@@ -1,13 +1,6 @@
 import { loadFlash } from 'sveltekit-flash-message/server';
 import { _limiter } from './api/audio/search/+server';
 
-//@ts-ignore env var exists at build and run time
-import { VERCEL_GIT_COMMIT_SHA } from "$env/dynamic/private";
-
 export const load = loadFlash(async (event) => {
     await _limiter.cookieLimiter?.preflight(event);
-
-    return {
-        gitDeploymentSha: VERCEL_GIT_COMMIT_SHA
-    }
 });
--- packages/site/src/routes/+layout.svelte
+++ packages/site/src/routes/+layout.svelte
@@ -17,9 +17,9 @@
 
 <header class="flex flex-col min-h-screen z-50">
 
-	<div class="absolute bottom-4 right-4 text-white text-opacity-25 text-xs z-[60]">
+	<!-- <div class="absolute bottom-4 right-4 text-white text-opacity-25 text-xs z-[60]">
 		Commit: {"AAAAAAAAAAA".slice(0, 7)}
-	</div>
+	</div> -->
 
 	{#if $flash}
 		<div class={`z-[99] animate-fadeInOut absolute top-[5rem] right-8 rounded-lg ${$flash.type === "success" ? "bg-[#02311b]" : "bg-[#3b0703]"}`}>
