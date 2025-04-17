af8d0b87329ea239c1e55338762a2f2e76edf507
choke-dev <admin@choke.dev>
2025-04-18 07:48:01 +0800

    OMFG REMOVE SHITTY DUPE

--- packages/site/src/routes/+page.svelte
+++ packages/site/src/routes/+page.svelte
@@ -29,7 +29,6 @@
 	let totalItems = $state(0); // Total number of items (total audios found)
 	let filters = $state<{ filters: { label: string; value: string; inputValue: string }[], type: 'and' | 'or' }>({ filters: [], type: 'and' });
 	let sort = $state<{ field: string, order: 'asc' | 'desc' } | null>(null);
-	let sort = $state<{ field: string, order: 'asc' | 'desc' } | null>(null);
 
 	async function handleSearch(event?: Event) {
 		event?.preventDefault();
