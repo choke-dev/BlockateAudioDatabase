0877b9d187892be47b23d601a8d05677cf8cd4ab
choke-dev <admin@choke.dev>
2025-07-01 02:17:32 +0800

    add more debugging info

--- packages/site/src/routes/+layout.server.ts
+++ packages/site/src/routes/+layout.server.ts
@@ -1,11 +1,13 @@
 import { loadFlash } from 'sveltekit-flash-message/server';
 import { _limiter } from './api/audio/search/+server';
-import { VERCEL_GIT_COMMIT_SHA } from '$env/static/private';
+import { VERCEL_GIT_COMMIT_SHA, VERCEL_ENV, VERCEL_DEPLOYMENT_ID } from '$env/static/private';
 
 export const load = loadFlash(async (event) => {
     await _limiter.cookieLimiter?.preflight(event);
 
     return {
-        deploymentCommitSHA: VERCEL_GIT_COMMIT_SHA || "0000000",
+        deploymentCommitSHA: VERCEL_GIT_COMMIT_SHA || "DEV",
+        deploymentEnvironment: VERCEL_ENV || "development",
+        deploymentID: VERCEL_DEPLOYMENT_ID || "N/A"
     };
 });
--- packages/site/src/routes/+layout.svelte
+++ packages/site/src/routes/+layout.svelte
@@ -1,54 +1,79 @@
 <script lang="ts">
-	
 	import { getFlash } from 'sveltekit-flash-message';
 	import { page } from '$app/state';
 
 	import LucideCircleAlert from '~icons/lucide/circle-alert';
 	import LucideCircleCheck from '~icons/lucide/circle-check';
+	import LucideInfo from '~icons/lucide/info';
 
+	import * as Dialog from '$lib/components/ui/dialog/index.js';
 	import Header from '$lib/components/ui/custom/Header.svelte';
-	import "../app.css";
+	import '../app.css';
 	let { children, data } = $props();
 
 	const flash = getFlash(page, {
 		clearAfterMs: 10900
 	});
-
-	const slicedDeploymentCommitSHA = data.deploymentCommitSHA ? data.deploymentCommitSHA.slice(0, 7) : '';
 </script>
 
-<header class="flex flex-col min-h-screen z-50">
-
-	<div class="fixed bottom-0 right-0 text-white text-opacity-25 text-xs z-[60]">
-		{slicedDeploymentCommitSHA}
+<header class="z-50 flex min-h-screen flex-col">
+	<div class="fixed bottom-2 left-3 z-[60] text-xs text-white text-opacity-25">
+		<Dialog.Root>
+			<Dialog.Trigger> <LucideInfo class="size-6" /> </Dialog.Trigger>
+			<Dialog.Content>
+				<Dialog.Header>
+					<Dialog.Title>Deployment Information</Dialog.Title>
+					<Dialog.Description>
+						<p>Deployment Environment: {data.deploymentEnvironment}</p>
+						<p>Current Deployment ID: {data.deploymentID}</p>
+						<p>
+							Current Deployment Commit SHA: 
+							<a class="underline underline-offset-2" href={`https://github.com/choke-dev/BlockateAudioDatabase/commit/${data.deploymentCommitSHA}`}>{data.deploymentCommitSHA}</a>
+						</p>
+					</Dialog.Description>
+				</Dialog.Header>
+			</Dialog.Content>
+		</Dialog.Root>
 	</div>
 
 	{#if $flash}
-		<div class={`z-[99] animate-fadeInOut absolute top-[5rem] right-8 rounded-lg ${$flash.type === "success" ? "bg-[#02311b]" : "bg-[#3b0703]"}`}>
-			<div class={`flex p-4 font-poppins ${$flash.type === "success" ? "text-[#8feab7]" : "text-[#fab4af]"}`}>
-				<div class="mr-3"> {#if $flash.type == "success"} <LucideCircleCheck class="h-6" /> {:else} <LucideCircleAlert class="h-6" /> {/if} </div>
+		<div
+			class={`absolute right-8 top-[5rem] z-[99] animate-fadeInOut rounded-lg ${$flash.type === 'success' ? 'bg-[#02311b]' : 'bg-[#3b0703]'}`}
+		>
+			<div
+				class={`flex p-4 font-poppins ${$flash.type === 'success' ? 'text-[#8feab7]' : 'text-[#fab4af]'}`}
+			>
+				<div class="mr-3">
+					{#if $flash.type == 'success'}
+						<LucideCircleCheck class="h-6" />
+					{:else}
+						<LucideCircleAlert class="h-6" />
+					{/if}
+				</div>
 				<div>
-					<h1 class="font-bold">{($flash.type as string).charAt(0).toUpperCase() + ($flash.type as string).slice(1)}</h1>
+					<h1 class="font-bold">
+						{($flash.type as string).charAt(0).toUpperCase() + ($flash.type as string).slice(1)}
+					</h1>
 					<p>{$flash.message}</p>
 				</div>
 			</div>
 		</div>
 	{/if}
 
-	<div class="fixed inset-0 h-full w-full bg-[radial-gradient(#241C1C_1px,transparent_1px)] [background-size:16px_16px] -z-50"></div>
- <!-- optimally, i would want this to calculate the color based on the background with a lighter shade, but i dont know how, if you're reading this and know how, please let me know -->
-	<div class="fixed top-0 inset-x-0 hidden md:block z-50">
+	<div
+		class="fixed inset-0 -z-50 h-full w-full bg-[radial-gradient(#241C1C_1px,transparent_1px)] [background-size:16px_16px]"
+	></div>
+	<!-- optimally, i would want this to calculate the color based on the background with a lighter shade, but i dont know how, if you're reading this and know how, please let me know -->
+	<div class="fixed inset-x-0 top-0 z-50 hidden md:block">
 		<Header />
 	</div>
-	<div class="flex-grow mt-0 mb-14 md:mt-14 md:mb-0">
+	<div class="mb-14 mt-0 flex-grow md:mb-0 md:mt-14">
 		{@render children()}
 	</div>
-	<div class="fixed bottom-0 inset-x-0 md:hidden z-50">
+	<div class="fixed inset-x-0 bottom-0 z-50 md:hidden">
 		<Header />
 	</div>
-
 </header>
 
 <!-- text color 8feab7 -->
 <!-- bg color 02311b -->
-
